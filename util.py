a7d1655017132bf6814006c393050bbd5e742d33
revaldyazura <91121018+revaldyazura@users.noreply.github.com>
2024-05-16 10:57:51 +0700

    Update util.py
    change limit to just hue

--- util.py
+++ util.py
@@ -6,19 +6,17 @@ def get_limits(color):
     c = np.uint8([[color]])  # BGR values
     hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
 
-    hue, saturation, value = hsvC[0][0]  # Get the HSV components
+    hue = hsvC[0][0][0]  # Get the hue value
 
-    # Define a range around the hue value
-    hue_lower = hue - 10 if hue > 10 else 0
-    hue_upper = hue + 10 if hue < 245 else 255
-
-    # Use the extracted saturation and value
-    saturation_lower = max(0, saturation - 30)  # Adjust based on color characteristics
-    saturation_upper = min(255, saturation + 30)
-    value_lower = max(0, value - 30)
-    value_upper = min(255, value + 30)
-
-    lowerLimit = np.array([hue_lower, saturation_lower, value_lower], dtype=np.uint8)
-    upperLimit = np.array([hue_upper, saturation_upper, value_upper], dtype=np.uint8)
+    # Handle red hue wrap-around
+    if hue >= 165:  # Upper limit for divided red hue
+        lowerLimit = np.array([hue - 10, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([180, 255, 255], dtype=np.uint8)
+    elif hue <= 15:  # Lower limit for divided red hue
+        lowerLimit = np.array([0, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([hue + 10, 255, 255], dtype=np.uint8)
+    else:
+        lowerLimit = np.array([hue - 10, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([hue + 10, 255, 255], dtype=np.uint8)
 
     return lowerLimit, upperLimit
