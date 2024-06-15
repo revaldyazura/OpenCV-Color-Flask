409d1e26a3fe3c151e367e6bc0fd4ddcb7346884
revaldyazura <raihanazura7@gmail.com>
2024-06-15 10:08:59 +0700

    add dilation

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from util import get_limits
 from flask import Flask, render_template, Response, request, jsonify
 from PIL import Image
 import cv2
+import numpy as np
 
 app = Flask(__name__) # initialize flask app
 
@@ -18,9 +19,9 @@ def index():
 @app.route('/add_color', methods=['POST'])
 def add_color():
     data = request.json
-    color_name = data['colorName']
-    bgr_values = data['bgrValues']
-    colors[color_name] = bgr_values
+    # color_name = data['colorName']
+    bgr_values = tuple(data['bgrValues'])
+    colors[bgr_values] = bgr_values
     return jsonify({'message': 'Color added successfully'})
 
 @app.route('/reset_colors', methods=['DELETE'])
@@ -31,6 +32,7 @@ def reset_colors():
     return jsonify({'message': 'Color values reset successfully'})
 
 def generate_frames():
+    kernel = np.ones((5, 5), np.uint8)
     while True:
         success, frame = camera.read()
         if not success:
@@ -38,15 +40,16 @@ def generate_frames():
         else:
             hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-            for color_name, color_value in colors.items():
-                lower_limit, upper_limit = get_limits(color=color_value)
+            for bgr_values in colors.values():
+                lower_limit, upper_limit = get_limits(color=bgr_values)
                 mask = cv2.inRange(hsvImage, lower_limit, upper_limit)
-                bbox = Image.fromarray(mask).getbbox()
+                dilated_mask = cv2.dilate(mask, kernel, iterations=1)
+                bbox = Image.fromarray(dilated_mask).getbbox()
 
                 if bbox is not None:
                     x1, y1, x2, y2 = bbox
-                    frame = cv2.rectangle(frame, (x1, y1), (x2, y2), color_value, 5)
-                    cv2.putText(frame, color_name, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color_value, 2)
+                    frame = cv2.rectangle(frame, (x1, y1), (x2, y2), bgr_values, 5)
+                    cv2.putText(frame, str(bgr_values), (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr_values, 2)
 
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
